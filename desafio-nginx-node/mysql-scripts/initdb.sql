56454b9e1da3c2bab9fbe39b3d85ca11ebf28aee
João Paulo <jooaoopauloo100@gmail.com>
2023-11-20 20:41:58 -0300

    criar tabela apenas se nao existir

--- desafio-nginx-node/mysql-scripts/initdb.sql
+++ desafio-nginx-node/mysql-scripts/initdb.sql
@@ -1,3 +1,3 @@
 USE fc_docker_desafio;
 
-CREATE TABLE people (id int not null auto_increment, name varchar(255), primary key (id));
+CREATE TABLE IF NOT EXISTS people (id int not null auto_increment, name varchar(255), primary key (id));
